f61dd702fa25e9b6415a379f14ba529c191d45e7
CadenZheng <51351005+CadenZheng@users.noreply.github.com>
2022-04-30 22:45:39 -0700

    Update greensc.v

--- DE1_SOC_D8M_LB_RTL/TMZ-181_Verilog/greensc.v
+++ DE1_SOC_D8M_LB_RTL/TMZ-181_Verilog/greensc.v
@@ -7,6 +7,14 @@ module greensc (
   output [7:0] out_g,
   output [7:0] out_b
 );
-  
+  wire [23:0] greeness;
+  wire [23:0] thresh;
+
+  assign greeness = in_g*(in_g-in_r)*(in_g-in_b);
+  assign thresh = 24'b0000_0001_0100_0011_1101_1010;
+ 
+  assign out_r = (greeness > thresh) ? 8'b0000_0000 : in_r;
+  assign out_g = (greeness > thresh) ? 8'b0000_0000 : in_g;
+  assign out_b = (greeness > thresh) ? 8'b0000_0000 : in_b;
   
 endmodule
