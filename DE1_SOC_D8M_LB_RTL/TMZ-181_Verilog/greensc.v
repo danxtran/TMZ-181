9a92255adda3f85f10ea88bfc8ea4b9b8efbfb6a
CadenZheng <51351005+CadenZheng@users.noreply.github.com>
2022-04-30 23:52:54 -0700

    green screen working! thresh:0.01

--- DE1_SOC_D8M_LB_RTL/TMZ-181_Verilog/greensc.v
+++ DE1_SOC_D8M_LB_RTL/TMZ-181_Verilog/greensc.v
@@ -7,11 +7,12 @@ module greensc (
   output [7:0] out_g,
   output [7:0] out_b
 );
-  wire [23:0] greeness;
-  wire [23:0] thresh;
+
+  wire signed [24:0] greeness;
+  wire signed [24:0] thresh;
 
   assign greeness = in_g*(in_g-in_r)*(in_g-in_b);
-  assign thresh = 24'b0000_0001_0100_0011_1101_1010;
+  assign thresh = 25'b0_0000_0001_0100_0011_1101_1010;
  
   assign out_r = (greeness > thresh) ? 8'b0000_0000 : in_r;
   assign out_g = (greeness > thresh) ? 8'b0000_0000 : in_g;
