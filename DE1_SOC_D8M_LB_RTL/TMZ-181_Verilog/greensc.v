524d4fcad66c81f3dfe92460febe00435077be71
Dan Tran <37347037+danxtran@users.noreply.github.com>
2022-05-25 16:30:28 -0700

    fixed code formatting

--- DE1_SOC_D8M_LB_RTL/TMZ-181_Verilog/greensc.v
+++ DE1_SOC_D8M_LB_RTL/TMZ-181_Verilog/greensc.v
@@ -1,27 +1,27 @@
 module greensc (
+  input [13:0] row,
+  input [13:0] col,
   input gsc_en,
   input [23:0] pixel_in,
   output reg [23:0] pixel_out,
   input [23:0] pass_in,
-	input [13:0] row,
-	input [13:0] col,
   output [23:0] pass_thru
 );
 	
-	wire [8:0] background;
-	wire [14:0] sumrc = row + col; 
-	mod mod360(.in(sumrc[10:0]),.out(background));
+wire [8:0] background;
+wire [14:0] sumrc = row + col; 
+mod mod360(.in(sumrc[10:0]),.out(background));
 	
 
 assign pass_thru = pass_in;
  
-  always @(*)begin
-		pixel_out = pixel_in;
-		if(gsc_en == 1'b1)begin
-			if(pixel_in[23:15] >= 9'd90 && pixel_in[23:15] <= 9'd150) begin
-				pixel_out[23:15] = background;
-				pixel_out[14:0] = 15'b1111_1111_1111_111;
-			end 
-		end
+always @(*)begin
+  pixel_out = pixel_in;
+  if(gsc_en == 1'b1)begin
+    if(pixel_in[23:15] >= 9'd90 && pixel_in[23:15] <= 9'd150) begin
+      pixel_out[23:15] = background;
+      pixel_out[14:0] = 15'b1111_1111_1111_111;
+    end 
   end
+end
 endmodule
